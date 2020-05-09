1d8cc5d20603d36e76073cdd06efe6b28adcaf11
Rohit <rohitmpaul@gmail.com>
2020-05-09 12:48:45 +0530

    Updates

---
+++ precache-manifest.d5474ce68bcc35d97cfb72528662dd05.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af7623d205adaf0bec9d0d82e8cad23f",
+    "url": "/learningredux/index.html"
+  },
+  {
+    "revision": "e7890c4f0c7343880d08",
+    "url": "/learningredux/static/js/2.e6e1be05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/learningredux/static/js/2.e6e1be05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c213c76bbb46e7d1887",
+    "url": "/learningredux/static/js/main.c6f1d32f.chunk.js"
+  },
+  {
+    "revision": "e7315ac9e1508bb63ff2",
+    "url": "/learningredux/static/js/runtime-main.6629d962.js"
+  }
+]);
